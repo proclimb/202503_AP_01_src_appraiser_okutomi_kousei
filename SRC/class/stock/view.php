69f34d9edb77ec737fed6d1b0efcaabfd683c5bf
okutomikousei <202503-AP-01-kousei-okutomi>
2025-05-19 11:45:29 +0900

    #40224/仕入管理/仕入登録画面で備考欄に1000文字入れて登録しようとするとサーバーエラーになる

--- SRC/class/stock/view.php
+++ SRC/class/stock/view.php
@@ -216,7 +216,7 @@ function subStockEditView($param)
 
 	<h1>仕入<?php print $param["purpose"] ?></h1>
 
-	<form name="form" id="form" action="index.php" method="get">
+	<form name="form" id="form" action="index.php" method="post">
 		<input type="hidden" name="act" />
 		<input type="hidden" name="sDel" value="<?php print $param["sDel"] ?>" />
 		<input type="hidden" name="sInsDTFrom" value="<?php print $param["sInsDTFrom"] ?>" />
