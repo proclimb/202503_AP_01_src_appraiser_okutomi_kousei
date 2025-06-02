1373dd9b7204a8a020d1a8fd633b701d33b94cb0
okutomikousei <202503-AP-01-kousei-okutomi>
2025-06-02 13:48:19 +0900

    #40516/物件管理/検索後に物件名の値が表示されていない

--- SRC/libArticle.php
+++ SRC/libArticle.php
@@ -58,7 +58,7 @@ function subArticle()
 				</tr>
 				<tr>
 					<th>物件名</th>
-					<td><input type="text" name="sArticle" value="<?php print $sRooms ?>" size="50" /></td>
+					<td><input type="text" name="sArticle" value="<?php print $sArticle ?>" size="50" /></td>
 					<th>キーBox番号</th>
 					<td><input type="text" name="sKeyBox" value="<?php print $sKeyBox ?>" size="30" /></td>
 				</tr>
