b5a9944e3ab875d1139dd5c24f26f2ce49017161
okutomikousei <202503-AP-01-kousei-okutomi>
2025-06-06 12:44:55 +0900

    #40639/売主物件/築年数が4桁でないと登録できない

--- SRC/js/sell.js
+++ SRC/js/sell.js
@@ -51,8 +51,8 @@ function fnSellEditCheck() {
 		alert('築年を入力してください');
 		return;
 	}
-	if (tmp.length != 4 || tmp.match(/[^0-9]+/)) {
-		alert('築年は4桁の半角数字で入力してください');
+	if (tmp.length > 4 || tmp.match(/[^0-9]+/)) {
+		alert('築年は4桁以内の半角数字で入力してください');
 		return;
 	}
 
