eb4839210ababb698708ba4d5b8ae38997163283
okutomikousei <202503-AP-01-kousei-okutomi>
2025-06-18 12:18:48 +0900

    #40809/案内管理/物件検索で複数の「＞」を押下すると、右側(案件情報登録候補)の背景色が全て白になる

--- SRC/js/guide.js
+++ SRC/js/guide.js
@@ -123,8 +123,8 @@ function fnGuideMove(muki, articleNo) {
 	// 色の再設定
 	$("#search tr").filter(":odd").find("td").attr("class", "list_td0");
 	$("#search tr").filter(":even").find("td").attr("class", "list_td1");
-	$("#regist tr").filter("odd").find(".td").attr("class", "list_td0");
-	$("#regist tr").filter("even").find(".td").attr("class", "list_td1");
+	$("#regist tr").filter(":odd").find("td").attr("class", "list_td0");
+	$("#regist tr").filter(":even").find("td").attr("class", "list_td1");
 }
 
 
